a36ec5c20c6426563cb3596a557e57e470d6b2ee
Sergey <pimka@ro.ru>
2016-10-11 12:02:11 +0400

    create main lib project sceleton

---
+++ FuncUnion/FuncUnion/OpaqueFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.Composition;
+using FuncUnion;
+
+namespace OpaqueFunctions
+{
+    [Export(typeof(Opaque1SinCos))]
+    [ExportMetadata("FuncName", "sin^2 + cos^2 = 1")]
+    [ExportMetadata("EquivalentIntConstant", 1)]
+    public class Opaque1SinCos : FunctionInterface
+    {
+        public double Body(double angle, int count)
+        {
+            double X = 1;
+            for (int i = 0; i < count; i++)
+            {
+                X *= Math.Sin(angle) * Math.Sin(angle) + Math.Cos(angle) * Math.Cos(angle);
+            }
+            return X;
+        }
+    }
+    
+}
---
+++ FuncUnion/FuncUnion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace FuncUnion
+{
+    class Program
+    {
+        [ImportMany(typeof(FunctionInterface))]
+        IEnumerable<Lazy<FunctionInterface, MetaDataInterface>> _stateRules;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+        void Run()
+        {
+            Compose();
+            Console.ReadKey();
+        }
+
+        private void Compose()
+        {
+            AssemblyCatalog catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.SatisfyImportsOnce(this);
+        }
+    }
+}
